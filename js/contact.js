623effbf2f134e14d329d26ea71053e206c366d2
Oke Edafe Great <greatedafeoke@gmail.com>
2024-03-30 09:37:03 +0100

    Added whatsapp functionalities

---
+++ js/contact.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const submitButton = document.getElementById("submit-button");
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const form = document.getElementById("form");
+
+    let url = "https://wa.me/2349057442227?text=";
+    for (const element of form.elements) {
+      if (element.tagName === "INPUT" || element.tagName === "TEXTAREA") {
+        url += element.name + ": " + element.value + "%0a";
+      }
+    }
+    window.open(url, "_blank");
+  });
+});
